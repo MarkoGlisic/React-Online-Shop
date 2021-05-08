035a76556eed4de6fb75b175ba800bcf81836724
Marko Glisic <38302481+MarkoGlisic@users.noreply.github.com>
2021-05-08 15:46:53 +0200

    Finished MyAdsComponent

--- src/components/MyAdsComponent.js
+++ src/components/MyAdsComponent.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import BreadcrumbComponent from './BreadcrumbComponent'
-import NavbarTop from './NavbarTop'
+import AdCard from "./AdCard";
+import BreadcrumbComponent from "./BreadcrumbComponent";
+import NavbarTop from "./NavbarTop";
+import { useData } from "../contexts/FirebaseDataContext";
+import { useAuth } from "../contexts/AuthContext";
 
 const MyAdsComponent = () => {
-    return (
-        <>
-            <NavbarTop/>
-            <BreadcrumbComponent path={'category'} name={"Categories"} id={'My Ads'}/>
-            <h1 style={{marginTop:'5rem'}}>Test</h1>
-        </>
-    )
-}
+  const { allAds } = useData();
+  const { currentUserName } = useAuth();
+  return (
+    <>
+      <NavbarTop />
+      <BreadcrumbComponent
+        path={"category"}
+        name={"Categories"}
+        id={"My Ads"}
+      />
+      {allAds.map((ad) => {
+        if (ad.adOwner === currentUserName) {
+          return (
+            <div key={ad.title}>
+              <AdCard
+                category={ad.category}
+                title={ad.adName}
+                price={ad.price}
+                imgURL={ad.imgURL}
+              />
+            </div>
+          );
+        }
+      })}
+    </>
+  );
+};
 
-export default MyAdsComponent
+export default MyAdsComponent;
